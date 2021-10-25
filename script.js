eac5cd25de39fbdd340a3530cbbf66ac64d539f2
Alex Silva <alexcssilva@gmail.com>
2021-10-25 14:39:49 -0300

    Desafio 04 concluido

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+let listUl = document.getElementById('pixel-board');
 
+for (let index = 0; index < 5; index += 1) {
+  let liLine = document.createElement('li');
+
+  let ulLIne = document.createElement('ul');
+  for (let index1 = 0; index1 < 5; index1 += 1) {
+    let liChild = document.createElement('li');
+    liChild.classList.add('pixel');
+    ulLIne.appendChild(liChild);
+  }
+  liLine.appendChild(ulLIne);
+  listUl.appendChild(liLine);
+}
+
+window.onload = () => {
+  let allPixel = document.querySelectorAll('.pixel');
+
+  for (let pixel of allPixel){
+    pixel.style.backgroundColor = 'white';
+  }
+alert(allPixel.length)
+};
