6379efb3e479d241114155a3c2d7ec2ef1b71494
Alex Silva <alexcssilva@gmail.com>
2021-10-25 16:33:26 -0300

    Desafio 06 concluido

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const listUl = document.getElementById('pixel-board');
 
 for (let index = 0; index < 5; index += 1) {
-  const liLine = document.createElement('li');
+  const liLine = document.createElement('div');
 
-  const ulLIne = document.createElement('ul');
+  const ulLIne = document.createElement('div');
   for (let index1 = 0; index1 < 5; index1 += 1) {
-    const liChild = document.createElement('li');
+    const liChild = document.createElement('div');
     liChild.classList.add('pixel');
     ulLIne.appendChild(liChild);
   }
@@ -20,3 +20,7 @@ window.onload = () => {
     pixel.style.backgroundColor = 'white';
   }
 };
+
+const colorPalette = document.getElementById('color-palette');
+
+colorPalette.firstElementChild.classList.add('selected');
