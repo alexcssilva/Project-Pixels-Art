e5ce77e8b075004865b73d45f2d607896ec3de56
Alex Silva <alexcssilva@gmail.com>
2021-10-25 23:03:30 -0300

    Desafio 08 concluido

--- script.js
+++ script.js
@@ -20,17 +20,35 @@ window.onload = () => {
     pixel.style.backgroundColor = 'white';
   }
   const colorPalette = document.getElementById('color-palette');
-colorPalette.firstElementChild.classList.add('selected');
+  colorPalette.firstElementChild.classList.add('selected');
 };
 
 let clickColor = document.querySelectorAll('.color');
 
 for (let index = 0; index < clickColor.length; index += 1) {
   clickColor[index].addEventListener('click', getColor);
-};
+}
 
-function getColor (evento) {
-  let selectColor = document.querySelector('.selected');
-  selectColor.classList.remove('selected');
+/* Desafio concluido e resolvido em grupo
+pelo os alunos George Lucas - Turma 17, Abner Sousa - Turma 17, Gabriel Medeiros, Joao Keuwe - Turma -17 */
+function getColor(evento) {
+  const classSelect = document.querySelector('.selected');
+  classSelect.classList.remove('selected');
   evento.target.classList.add('selected');
-};
+}
+
+let selectColor = 'black';
+
+for (let color of clickColor) {
+  color.addEventListener('click', function (element) {
+    selectColor = element.target.id;
+  })
+}
+
+let pixelColors = document.querySelectorAll('.pixel');
+
+for (let pixels of pixelColors) {
+  pixels.addEventListener('click', function (elementPixel) {
+    elementPixel.target.style.backgroundColor = selectColor;
+  });
+}
